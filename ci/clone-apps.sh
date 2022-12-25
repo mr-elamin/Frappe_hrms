03b440ce3a099bb76c789eba96ac465926cc6ebe
mr-elamin <105249992+mr-elamin@users.noreply.github.com>
2022-12-25 14:21:21 +0100

    Update clone-apps.sh

--- ci/clone-apps.sh
+++ ci/clone-apps.sh
@@ -18,7 +18,7 @@
 
 # Following are public repositories.
 
-git clone --depth 1 --branch develop https://github.com/frappe/hrms.git repos/hrms
-git clone --depth 1 --branch develop https://github.com/frappe/desk.git repos/desk
-git clone --depth 1 --branch develop https://github.com/frappe/lms.git repos/lms
+git clone --depth 1 --branch develop https://github.com/frappe/hrms.git ~/repos/hrms
+git clone --depth 1 --branch develop https://github.com/frappe/desk.git ~/repos/desk
+git clone --depth 1 --branch develop https://github.com/frappe/lms.git ~/repos/lms
 
